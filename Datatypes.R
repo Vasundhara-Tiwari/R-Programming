50406604ab0c2b80682a603e3e5dad5767152146
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-09 22:01:24 +0530

    datatypes

---
+++ Datatypes.R
@@ -0,0 +1,16 @@
+{
+  v <- TRUE 
+  print(class(v))
+  
+  v <- 23.5
+  print(class(v))
+  
+  v <- 2L
+  print(class(v))
+  
+  v <- 2+5i
+  print(class(v))
+  
+  v <- "TRUE"
+  print(class(v))
+}
