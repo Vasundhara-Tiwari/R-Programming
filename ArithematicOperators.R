ef0f0dce8071fa4d8bc0971154a32e65125773b4
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-09 22:02:59 +0530

    arithematic operators in R

---
+++ ArithematicOperators.R
@@ -0,0 +1,11 @@
+{
+  v <- c( 2,5.5,6)
+  t <- c(8, 3, 4)
+  print(v+t)      #Adds two vectors
+  print(v-t)      #Subtracts second vector from the first
+  print(v*t)      #Multiplies both vectors
+  print(v/t)      #Divide the first vector with the second
+  print(v%%t)     #Give the remainder of the first vector with the second
+  print(v%/%t)    #The result of division of first vector with second (quotient)
+  print(v^t)      #The first vector raised to the exponent of second vector
+}
