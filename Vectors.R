a28548ebb9a2a915396de9be70f9f86b65f4c177
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-09 22:07:23 +0530

    Vectors datatype

---
+++ Vectors.R
@@ -0,0 +1,13 @@
+{
+  v1 <- c(3,1,TRUE,2+3i)
+  v2 <<- c(3,1,TRUE,2+3i)
+  v3 = c(3,1,TRUE,2+3i)
+  print(v1)
+  print(v2)
+  print(v3)
+  
+  c(3,1,TRUE,2+3i) -> v4
+  c(3,1,TRUE,2+3i) ->> v5 
+  print(v4)
+  print(v5)
+}
