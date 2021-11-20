1ad1edf63393438e82d446085a79978c2248ccf1
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-20 19:49:44 +0530

    Conditional statements in R

---
+++ ConditionalStatements.R
@@ -0,0 +1,13 @@
+{
+  var.1 = readline()
+  x = as.integer(var.1)
+  var.2 = readline()
+  y = as.integer(var.2)
+  if(x > y)
+  {
+    print("X is greater than Y")
+  } else if(y > x) {
+    print("Y is greater than X")
+  }else
+    print("Both are equal")
+}
