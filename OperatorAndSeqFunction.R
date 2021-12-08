c69261da5da6088e322964224a70a1b7b6ffdb07
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-08 22:07:34 +0530

    Operator and sequence function

---
+++ OperatorAndSeqFunction.R
@@ -0,0 +1,12 @@
+{
+  x = 1:15
+  print("New vector using : operator-")
+  print(x)
+  print("New vector using seq() function-")
+  print("Specify step size:")
+  y = seq(1, 3, by=0.3)  
+  print(y)
+  print("Specify length of the vector:")
+  z = seq(1, 5, length.out = 6)
+  print(z)
+}
