dc09f6ab5907aa304a88e7fd8d5fcc8a4ee8ebca
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-08 21:37:00 +0530

    program to print factorial of the given number

---
+++ Factorial.R
@@ -0,0 +1,16 @@
+{
+  x <- as.integer(readline(prompt = "Enter number"));
+  factorial <- 1;
+  if(x < 0){
+    print("given number is negative");
+  }
+  else if(x == 0){
+    print("1");
+  }
+  else {
+    for(i in 1:x){
+      factorial = factorial * i;
+    }
+  }
+  print(paste("factorial of "))
+}
