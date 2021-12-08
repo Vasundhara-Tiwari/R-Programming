5a07e2720cf1b77b5e687e7af0cc62408851b2f0
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-08 22:01:51 +0530

    NthHighestVector

---
+++ NthHighestVector.R
@@ -0,0 +1,18 @@
+{
+  x = c(10, 20, 30, 20, 20, 25, 9, 26)
+  print("Original Vectors:")
+  print(x)
+  print("nth highest value in a given vector:")
+  print("n = 1")
+  n = 1
+  print(sort(x, TRUE)[n])
+  print("n = 2")
+  n = 2
+  print(sort(x, TRUE)[n])
+  print("n = 3")
+  n = 3
+  print(sort(x, TRUE)[n])
+  print("n = 4")
+  n = 4
+  print(sort(x, TRUE)[n])
+}
