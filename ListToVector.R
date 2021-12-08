256fb208b007e23be132f46649ff68bf21b69c6b
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-08 22:08:12 +0530

    adding a program to convert list to vector

---
+++ ListToVector.R
@@ -0,0 +1,16 @@
+{
+  n1 = list(1,2,3)
+  c1 = list(4,5,6)
+  print("Original lists:")
+  print(n1)
+  print(c1)
+  print("Convert the lists to vectors:")
+  v1 = unlist(n1)
+  v2 = unlist(c1)
+  print(v1)
+  print(v2)
+  print("Add two vectors:")
+  v = v1 + v2
+  print("New vector:")
+  print(v)
+}
